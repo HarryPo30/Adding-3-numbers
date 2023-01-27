d7cf2003535c7e2a70d04ecb80c7864dacc96e1f
HarryPo30 <123658884+HarryPo30@users.noreply.github.com>
2023-01-27 15:47:02 +0530

    Add files via upload

---
+++ Add3numbers.java
@@ -0,0 +1,10 @@
+public class Add3numbers {
+    public static void main(String[] args){
+        int num1=50;
+        int num2=40;
+        int num3=60;
+        int sum= num1+num2+num3;
+        System.out.println("The sum of these numbers is:- ");
+        System.out.println(sum);
+    }
+}
